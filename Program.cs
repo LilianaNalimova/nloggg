bf23d1eddb772c7c969d208cdcaf5f5989676f5f
LilianaNalimova <150796146+LilianaNalimova@users.noreply.github.com>
2024-01-28 21:11:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NLog;
+
+public class Program
+{
+    private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Введите число:");
+            int num = int.Parse(Console.ReadLine());
+
+            ExampleClass example = new ExampleClass();
+            example.MultiplyInfiniteLoop(num);
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, "Произошла ошибка: {0}", ex.Message);
+        }
+    }
+
+
+    public class ExampleClass
+    {
+        public void MultiplyInfiniteLoop(int num)
+        {
+            try
+            {
+                while (true)
+                {
+                    checked
+                    {
+                        num *= num;
+                    }
+                }
+            }
+            catch (OverflowException ex)
+            {
+                logger.Error(ex, "Произошло переполнение при умножении {0} в бесконечном цикле", num);
+                throw;
+            }
+        }
+    }
+}
